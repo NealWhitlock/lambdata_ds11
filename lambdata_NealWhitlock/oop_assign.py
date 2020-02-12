a75880704d294d3c0248941c53e992db534a2df0
NealWhitlock <NealWhitlock@users.noreply.github.com>
2020-02-12 16:56:24 -0700

    fixed na_filler in oop_assign

--- lambdata_NealWhitlock/oop_assign.py
+++ lambdata_NealWhitlock/oop_assign.py
@@ -11,15 +11,15 @@ class na_filler(df, n=3):
         self.df = df
 
     
-    for col in df.describe().columns: # Look at only numeric columns
-        nums = list(zip(np.where(df.isna())[0],np.where(df.isna())[1])) # Combine indices into list
+        for col in df.describe().columns: # Look at only numeric columns
+            nums = list(zip(np.where(df.isna())[0],np.where(df.isna())[1])) # Combine indices into list
 
-    for i in range(len(nums)): # Loop through all of the NaN locations
-        values = [] # This will store nearby values in the column
-        for j in range(-n,n): # Range to grab nearby values in column
-            if (nums[i][0]+j) >= 0 and (nums[i][0]+j) < (len(df)): # Make sure we stay in bounds
-                if not (pd.isna(df.iloc[nums[i][0]+j][nums[i][1]])): # Skip if the value is NaN
-                    values.append(df.iloc[nums[i][0]+j][nums[i][1]]) # Add to to list
-        df.iat[nums[i][0],nums[i][1]] = sum(values)/len(values) # Replace the NaN with the mean
-        
-    return df
+        for i in range(len(nums)): # Loop through all of the NaN locations
+            values = [] # This will store nearby values in the column
+            for j in range(-n,n): # Range to grab nearby values in column
+                if (nums[i][0]+j) >= 0 and (nums[i][0]+j) < (len(df)): # Make sure we stay in bounds
+                    if not (pd.isna(df.iloc[nums[i][0]+j][nums[i][1]])): # Skip if the value is NaN
+                        values.append(df.iloc[nums[i][0]+j][nums[i][1]]) # Add to to list
+            df.iat[nums[i][0],nums[i][1]] = sum(values)/len(values) # Replace the NaN with the mean
+            
+        return df
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="lambdata-NealWhitlock",
-    version="0.1.5",
+    version="0.1.6",
     author="NealWhitlock",
     author_email="nealwhitlock@gmail.com",
     description="Example package for lambda school DS Unit 3",
